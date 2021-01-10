100f5b674e6ee7131a899c2b0541a90cf679b15b
guoli <guoli@trustfar.cn>
2021-01-10 20:14:58 +0800

    恢复版本

---
+++ src/api/whitelist/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var whitelist = ['/auth'];
+var _default = whitelist;
+exports["default"] = _default;
