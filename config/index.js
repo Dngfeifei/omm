aaa660ebb67ebd8fd7a6d5e605ea82a593da8907
guoli <guoli@trustfar.cn>
2021-01-15 16:53:02 +0800

    修改 修改

--- config/index.js
+++ config/index.js
@@ -3,10 +3,10 @@ var path = require('path')
 var merge = require('webpack-merge')
 var buildconfig = {
   env: require('./prod.env'),
-  index: path.resolve(__dirname, '../dist/index.html'),
-  assetsRoot: path.resolve(__dirname, '../dist'),
+  index: path.resolve(__dirname, '../omsweb/index.html'),
+  assetsRoot: path.resolve(__dirname, '../omsweb'),//path.resolve(__dirname, '../dist'),
   assetsSubDirectory: 'static',
-  assetsPublicPath: '/',
+  assetsPublicPath: './',
   productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
