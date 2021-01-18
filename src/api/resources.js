d6825bea1c7be2ad1af56b6b56531a3bcf1fb810
yangyunpeng <yangyp@trustfar.cn>
2021-01-18 11:36:10 +0800

    角色和资源模块代码上传

---
+++ src/api/resources.js
@@ -0,0 +1,30 @@
+import http from './index'
+/** 
+ * 系统管理
+ * 资源管理接口
+ **/
+// 资源树查询
+export const GetResourceTree = _ => {
+	return http.fetchGet(`/sysResources/tree`, true)
+}
+// 新增资源
+export const AddResource = (params = {}) => {
+	return http.fetchPost(`/sysResources`, params)
+}
+// 编辑资源
+export const EditResource = (params = {}) => {
+	return http.fetchPut(`/sysResources`, params)
+}
+// 删除资源
+export const DelResource = (params = {}) => {
+	return http.fetchDelete(`/sysResources`, params)
+}
+// 查询资源详情
+export const GetResourceInfo = (params) => {
+	return http.fetchGet(`/sysResources/${params}`)
+}
+// 查询下拉框字典
+export const GetSysDictsInfo = (params) => {
+	return http.fetchGet(`sysDicts/${params}/sysDictItems`)
+	
+}
