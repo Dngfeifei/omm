06c97b1bd4d77b789f3f539c8d54446586b71216
guoli <guoli@trustfar.cn>
2021-03-17 17:22:35 +0800

    郭立修调整错误提示页面

--- src/page/error/index.jsx
+++ src/page/error/index.jsx
@@ -1,5 +1,8 @@
 
 import React from 'react'
+import {
+	hashHistory
+} from 'react-router'
 
 class ErrorBoundary extends React.Component{
     constructor(props){
@@ -11,10 +14,24 @@ class ErrorBoundary extends React.Component{
         this.setState({hasError:true});
         // logErrorToMyService(error,info);
     }
-
+    handleClick = ()=>{
+        hashHistory.push('/home') //开发模式下不经过改跳转
+    }
     render(){
         if(this.state.hasError){
-            return <h1>页面发生了错误</h1>
+            return (
+                <div>
+                    <div style={{ textAlign: 'center', paddingTop: 60 }}>
+                        <div style={{ textAlign: 'center' }}>
+                            <p style={{ fontSize: 150, padding: 10,margin:0 }}>error</p>
+                            <h2 style={{ fontSize: '1.5rem', lineHeight: '2rem', marginBottom: 10 }}>糟糕，页面出错了！</h2>
+                        </div>
+                    </div>
+                    <div style={{ textAlign: 'center' }}>
+                        <a href="" style={{ color: '#636363' }}onClick={this.handleClick}>返回首页</a>
+                    </div>
+                </div>
+            )
         }
         return this.props.children;
     }
