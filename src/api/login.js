fe8e515a3d1ff1a86cf18ecca86aca77cf9b3bbc
靳晓岚 <jinxl@trustfar.cn>
2021-03-30 16:57:49 +0800

    修改登录接口请求方式

--- src/api/login.js
+++ src/api/login.js
@@ -19,7 +19,7 @@ export const logout = (params = {}) => {
 
 // 首次登录修改密码
 export const changepass = (params = {}) => {
-	return http.fetchGet(`/updatePass`, params)
+	return http.fetchPost(`/updatePass`, params)
 }
 
 
