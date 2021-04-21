3a52a1d7644cf88abca3f54fa62fb411d3fe7ce2
yangyunpeng <yangyp@trustfar.cn>
2021-04-21 11:40:43 +0800

    宏观风险和宏观风险汇总页面调整

--- src/api/serviceMain.js
+++ src/api/serviceMain.js
@@ -27,16 +27,38 @@ export const SqtBaseupload = (params = {}) => {
 	return http.fetchPost(`/biSqtBase/upload`, params,true)
 }
 
-// 服务计划表---附表数据查询接口
+// 服务计划表---服务区域附表数据查询接口
 export const getAssistant = (params = {}) => {
-	return http.fetchGet(`/biSqtBase/getAssistant`, params)
+	return http.fetchGet(`biSqtBase/getAssistant`, params)
 }
-// 服务计划表---附表数据提交接口
+// 服务计划表---服务区域附表数据提交接口
 export const PostaddAssistant = (params = {}) => {
-	return http.fetchPost(`/biSqtBase/addAssistant`, params,true)
+	return http.fetchPost(`biSqtBase/addAssistant`, params, true)
 }
+
+
+// 服务计划表---宏观风险附表数据查询接口
+export const getMacroRisk = (params = {}) => {
+	return http.fetchGet(`biSqtMacroRisk`, params)
+}
+// 服务计划表---宏观风险附表数据提交接口
+export const PostMacroRisk = (params = {}) => {
+	return http.fetchPost(`biSqtMacroRisk`, params, true)
+}
+
+
+// 服务计划表---宏观风险汇总附表数据查询接口
+export const getMacroRiskSum = (params = {}) => {
+	return http.fetchGet(`biSqtMacroRiskSum`, params)
+}
+// 服务计划表---宏观风险汇总附表数据提交接口
+export const PostMacroRiskSum = (params = {}) => {
+	return http.fetchPut(`biSqtMacroResourceSum`, params, true)
+}
+
 
 // 服务计划表---微观风险数据提交接口
 export const PostaddMicroRisk = (params = {}) => {
 	return http.fetchPost(`/microRisk/add`, params,true)
 }
+
