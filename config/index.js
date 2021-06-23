3762364638c3e77288877c9194a1f455e62189e0
guoli <guoli@trustfar.cn>
2021-06-23 09:29:43 +0800

    郭立提交config.index

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ var buildconfig = {
   // Surge or Netlify already gzip all static assets for you.
   // Before setting to `true`, make sure to:
   // npm install --save-dev compression-webpack-plugin
-  productionGzip: false,
+  productionGzip: true,
   productionGzipExtensions: ['js', 'css'],
   // Run the build command with an extra argument to
   // View the bundle analyzer report after build finishes:
