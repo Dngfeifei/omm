d7a9535eabb6ef016cae4b96eb0407210da68373
GuoX0220 <guox0220@foxmail.com>
2021-07-05 15:27:50 +0800

    表单新增相关问题

--- src/api/database.js
+++ src/api/database.js
@@ -1,6 +1,6 @@
 import http from './index'
 
-const REMOTE_URL = 'http://152.136.121.201:8080/jeeplus-vue'
+const REMOTE_URL = ''
 
 export const getDataSource = (params) => http.fetchGet(REMOTE_URL + `/database/datalink/dataSource/treeData2`)
 
