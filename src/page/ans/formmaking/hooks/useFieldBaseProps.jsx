5b99e784e749e3c53a7ea6dfd93214b8e57e3f36
GuoX0220 <guox0220@foxmail.com>
2021-07-29 18:23:00 +0800

    project

--- src/page/ans/formmaking/hooks/useFieldBaseProps.jsx
+++ src/page/ans/formmaking/hooks/useFieldBaseProps.jsx
@@ -1,16 +1,16 @@
 import { useMemo, useContext, useEffect } from 'react'
 import formRenderContext from '@/page/ans/formmaking/lib/FormRender/formRenderContext';
 import FormDesignContenxt from '@/page/ans/formmaking/lib/formDesignContext';
+import http from '/api/index'
 
 export default function useFieldBaseProps(
-  control,
-  formConfig,
-  isDirectValue = false,
-  inTable=false,
-  onChange) {
+    control,
+    formConfig,
+    isDirectValue = false,
+    inTable=false,
+    onChange) {
   const { options } = control;
   const { state, updateValue, dispatch } = useContext(formRenderContext);
-  console.log(state);
   const changeFn = inTable ? onChange: updateValue
   const handleChange = evt => {
     const value = isDirectValue ? evt : evt.target.value
@@ -30,6 +30,7 @@ export default function useFieldBaseProps(
     if (!window.global) {
       window.global = {}
     }
+    window.global.http = http
     window.global.updateValue = changeFn
     window.global.updateControl = (newCtrl) => {
       state.formModel = state.formModel.map(ctrl => {
