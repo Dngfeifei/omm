d38a80a2a9c0c175b900b7d1294445015dc57226
yangyunpeng <yangyp@trustfar.cn>
2021-08-03 10:15:18 +0800

    服务计划表服务区域附表添加177电话校验项

--- src/components/workorder/SQT/serviceArea/member.jsx
+++ src/components/workorder/SQT/serviceArea/member.jsx
@@ -188,7 +188,7 @@ class Member extends Component {
     }
     // 电话框失去焦点
     onBlurTel = ({ target }) => {
-        let reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+        let reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9]|177)\d{8}$/
         let val = target.value;
         let result = reg.test(val)
         if (!result) {
@@ -277,7 +277,7 @@ class Member extends Component {
                     }
                 }
                 if (item == "mobilePhone") {
-                    let reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}|177$/
+                    let reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9]|177)\d{8}$/
                     let val = el[item];
                     let state = reg.test(val)
                     if (!state) {
